574fbbe92d6d41b7619329feb79b181198a9a487
Maxim Zemskov <maxim.zemskov@gmail.com>
2019-12-05 21:59:51 +0200

    оплата - в догонку

---
+++ src/main/java/com/pelengator/server/mobile/rest/entity/response/payment/PaymentStatusDataResponse.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2019 Spring-Pro
+ * Moscow, Russia
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of Spring-Pro. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Spring-Pro.
+ *
+ * Author: Maxim Zemskov, https://www.linkedin.com/in/mzemskov/
+ */
+
+package com.pelengator.server.mobile.rest.entity.response.payment;
+
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.annotations.Since;
+import com.pelengator.server.mobile.rest.entity.BaseEntity;
+
+public class PaymentStatusDataResponse extends BaseEntity {
+
+    @Since(1.0)
+    @SerializedName("period")
+    private Integer period;
+
+    public Integer getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(Integer period) {
+        this.period = period;
+    }
+}
